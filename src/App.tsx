7c2c8b5e23192876d8d86010713f6e25a1c646b4
ttabi <ttabiryu@gmail.com>
2026-04-18 04:05:03 +0900

    브랜드 타이틀 및 파비콘 커스텀

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import { useState, useEffect, useRef } from 'react';
 import { motion, useScroll, useTransform, AnimatePresence } from 'motion/react';
 
